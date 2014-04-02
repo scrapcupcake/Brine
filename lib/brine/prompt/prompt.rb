3400407215be650fef15e70ffc64978e533046d3
Hunter Thomas <archangelq@gmail.com>
2014-04-02 10:14:16 -0700

    Got responses back.

--- lib/brine/prompt/prompt.rb
+++ lib/brine/prompt/prompt.rb
@@ -1,28 +1,38 @@
 require "erb"
 
 module Prompt
+
+  def get_javascript_result(value)
+    Capybara.current_session.evaluate_script(value)
+  end
+
   def Prompt(scenario)
     message = scenario.steps.collect do |step|
       step == scenario.current_step ? ">> #{step}<br>" : "#{step}<br>"
     end.join("\n")
     message += "Step passes?<br>"
 
-    iframe = ERB.new(File.read(File.join(File.dirname(__FILE__),"template.erb"))).result(binding)
-    iframe = iframe.split("\n").collect{|line| "\"#{line}\"+\n"}.join("")
+    html_template = ERB.new(File.read(File.join(File.dirname(__FILE__),"template.erb"))).result(binding)
+    html_template = html_template.split("\n").collect{|line| "\"#{line}\"+"}.join("")
 
     script =     <<-eos
-      document.lastChild.innerHTML += #{iframe} "";
+      var element = document.createElement('div');
+      element.id = "BrineTestPanel";
+      element.innerHTML = #{html_template} "";
+      document.body.appendChild(element);
     eos
 
     Capybara.current_session.execute_script(script)
+
     finished = false
-    result = nil
     until finished
       sleep 0.1
-      finished = Capybara.current_session.evaluate_script("window.finished")
-      result = Capybara.current_session.evaluate_script("window.result")
+      finished = get_javascript_result("window.BrineTestFinished")
     end
 
-    result.should be_true, "Manual tester failed the test"
+    result = get_javascript_result("window.BrineTestResult")
+    reason = get_javascript_result("window.BrineTestReason")
+
+    result.should be_true, "Manual tester said: #{reason}"
   end
 end
