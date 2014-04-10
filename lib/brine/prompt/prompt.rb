5e7a45607673cb6af64c7092c1bc9fc36b9dff17
Hunter Thomas <archangelq@gmail.com>
2014-04-10 15:31:50 -0700

    Omg got it freaking working

--- lib/brine/prompt/prompt.rb
+++ lib/brine/prompt/prompt.rb
@@ -2,8 +2,29 @@ require "erb"
 
 module Prompt
 
-  def get_javascript_result(value)
+  def exec_script(value)
     Capybara.current_session.evaluate_script(value)
+  end
+
+  def require_url
+    "//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.11/require.js"
+  end
+
+  def brine_url
+    "https://gist.githubusercontent.com/scrapcupcake/10428904/raw/fa10ca7a14634ac06167536dc86b72c39164e389/brine.js"
+  end
+
+  def add_src(url)
+    script = <<-eos
+    head = document.getElementsByTagName('head')[0];
+    script = document.createElement('script');
+    script.src = '#{url}';
+    head.appendChild(script);
+    eos
+  end
+
+  def add_src_exec(url)
+    add_src(url).split(/\n/).each { |line| exec_script(line) }
   end
 
   def Prompt(scenario)
@@ -12,26 +33,16 @@ module Prompt
     end.join("\n")
     message += "Step passes?<br>"
 
-    html_template = ERB.new(File.read(File.join(File.dirname(__FILE__),"template.erb"))).result(binding)
-    html_template = html_template.split("\n").collect{|line| "\"#{line}\"+"}.join("")
-
-    script =     <<-eos
-      var element = document.createElement('div');
-      element.id = "BrineTestPanel";
-      element.innerHTML = #{html_template} "";
-      document.body.appendChild(element);
-    eos
-
-    Capybara.current_session.execute_script(script)
+    add_src_exec(brine_url)
 
     finished = false
     until finished
       sleep 0.1
-      finished = get_javascript_result("window.BrineTestFinished")
+      finished = exec_script("window.BrineTestFinished")
     end
 
-    result = get_javascript_result("window.BrineTestResult")
-    reason = get_javascript_result("window.BrineTestReason")
+    result = exec_script("window.BrineTestResult")
+    reason = exec_script("window.BrineTestReason")
 
     result.should be_true, "Manual tester wrote: #{reason}"
   end
